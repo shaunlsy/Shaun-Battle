cda9d3b8c4432e1fa70fcad14100ab63ff15ba28
cpcwood <cpcwood@outlook.com>
2020-01-06 15:41:37 +0000

    Cat with a boarder

--- lib/app.rb
+++ lib/app.rb
@@ -19,16 +19,5 @@ get '/secret3' do
 end
 
 get '/cat' do
-  "<div><img src='http://bit.ly/1eze8aE'><div>"
+  erb :cat
 end
-
-div {
-  border: 0.5rem outset pink;
-  outline: 0.5rem solid khaki;
-  box-shadow: 0 0 0 2rem skyblue;
-  border-radius: 12px;
-  font: bold 1rem sans-serif;
-  margin: 2rem;
-  padding: 1rem;
-  outline-offset: 0.5rem;
-}
